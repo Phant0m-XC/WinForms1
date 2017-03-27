6f0a4d0b330ed5a15ff2361964defe71b94abcc5
Ph@nt0m-X <phantom-x@inbox.ru>
2017-03-27 17:01:53 +0300

    1 - задача

--- DZ2/WindowsFormsApplication1/Form1.Designer.cs
+++ DZ2/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openfile = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 219);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(528, 31);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // openfile
+            // 
+            this.openfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.openfile.Location = new System.Drawing.Point(12, 190);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(75, 23);
+            this.openfile.TabIndex = 1;
+            this.openfile.Text = "OpenFile";
+            this.openfile.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(528, 172);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(552, 262);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.openfile);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- DZ2/WindowsFormsApplication1/Form1.cs
+++ DZ2/WindowsFormsApplication1/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
 
 namespace WindowsFormsApplication1
 {
@@ -15,6 +17,27 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            openfile.Click += openfile_Click;
+        }
+
+        private void openfile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            using (FileStream file = File.Open(openFileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = (int) file.Length;
+                progressBar1.Value = 0;
+                byte[] quant = new byte[1];
+                for(int i = 0; i < file.Length; i++)
+                {
+                    file.Read(quant, 0, 1);
+                    textBox1.Text += Encoding.Default.GetString(quant);
+                    progressBar1.Value++;
+                    Thread.Sleep(1);
+                }
+                file.Close();
+            }
         }
     }
 }
