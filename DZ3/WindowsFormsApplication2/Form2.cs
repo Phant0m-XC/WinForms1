a05a307d800a7b77f32a3beb32b1815e202e2e30
Ph@nt0m-X <phantom-x@inbox.ru>
2017-04-21 22:04:49 +0300

    DZ3 2-nd exercise add 2 forms

---
+++ DZ3/WindowsFormsApplication2/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form2 : Form
+    {
+        private Product prod;
+        public Form2(Product prod)
+        {
+            InitializeComponent();
+            this.prod = prod;
+            textBox1.Text = prod.Name;
+            textBox2.Text = prod.Charactiristics;
+            textBox3.Text = prod.Description;
+            textBox4.Text = prod.Price.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != null && textBox1.Text != "" &&
+                textBox2.Text != null && textBox2.Text != "" &&
+                textBox3.Text != null && textBox3.Text != "" &&
+                textBox4.Text != null && textBox4.Text != "")
+                {
+                    prod.Name = textBox1.Text;
+                    prod.Charactiristics = textBox2.Text;
+                    prod.Description = textBox3.Text;
+                    prod.Price = Convert.ToDouble(textBox4.Text);
+                }
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ DZ3/WindowsFormsApplication2/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Charactiristics { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public Product()
+        {
+            Name = null;
+            Charactiristics = null;
+            Description = null;
+            Price = 0;
+        }
+    }
+}
